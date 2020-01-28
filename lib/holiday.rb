83c519ee97db1cf17988a328c8fb172d141daf6f
Roberto Livi <robertomlivi@outlook.com>
2020-01-28 20:34:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,6 @@ def all_holidays_with_bbq(holiday_hash)
    holiday_hash.map do |season, holiday|
      holiday.map do |holidays, supply|
        holidays if supply.include?("BBQ")
-       binding.pry
      end
    end.flatten.compact
  end
